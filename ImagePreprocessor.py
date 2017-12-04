6a8b11f3b52e2d173bd42a98c682b6937b2d6231
patryk <patryk@outlook.ie>
2017-12-04 16:28:29 +0000

    Prototype

--- ImagePreprocessor.py
+++ ImagePreprocessor.py
@@ -8,8 +8,8 @@ class ImagePreprocessor:
     def __init__(self):
         pass
 
-
-    def rename_images(self, directory):
+    @staticmethod
+    def resize_images(directory):
 
         image_array = []
         print(directory)
@@ -18,18 +18,16 @@ class ImagePreprocessor:
             r = 150.0 / image.shape[1]
             dim = (150, int(image.shape[0] * r))
             resize = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+            r = 50.0 / image.shape[0]
+            dim = (int(image.shape[1] * r), 50)
+            resize = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
             face = np.array(resize, 'uint8')
             image_array.append(face)
-
 
         return image_array
 
 
-
-
-test = ImagePreprocessor()
-
-arr = test.rename_images("/home/patryk/Desktop/pictures")
+arr = ImagePreprocessor.resize_images("/home/patryk/Desktop/pictures")
 cv2.imshow("Resized", arr[0])
 print(type(arr))
 cv2.waitKey(0)
